72520556241a1369e51b96f3fbf227a3298adae7
makcmoscow <makcmoscow@gmail.com>
2018-05-24 01:27:23 +0300

    let's start to write LOOOGS

---
+++ logger.py
@@ -0,0 +1,18 @@
+
+path = 'new.log'
+y = (1,2,3,4,5,6)
+def logger(func):
+    def wrapped(*args, **kwargs):
+        with open(path, 'a') as f:
+            r = func(*args, **kwargs)
+            f.write('function {} did {}'.format(func.__name__, r)+'\n')
+            return r
+    return wrapped
+
+@logger
+def square(x):
+    return x**x
+
+
+for x in y:
+    square(x)
--- server.py
+++ server.py
@@ -18,44 +18,6 @@ def parser(): #It returns IP address and port if they are was given
     return addr, port
 
 
-# def _chk_ip_value(value):
-#     err_text = 'IP address supposed to be 4 integer number separated by ".", not {}'
-#     test_value = value.split('.')
-#     if len(test_value) != 4:
-#         print(err_text.format(value))
-#     else:
-#         counter = 0
-#         for x in test_value:
-#             try:
-#                 x = int(x)
-#                 if x == 0 and counter == 0:
-#                     print('IP address couldn\'t be started of zero')
-#                     return False
-#             except ValueError:
-#                 print(err_text.format(value))
-#                 return False
-#             else:
-#                 if int(x) < 0 or int(x) > 254:
-#                     print(err_text.format(value))
-#                     return False
-#                 counter += 1
-#         return True
-
-
-# def _chk_port_value(value):
-#     try:
-#         port = int(value)
-#     except ValueError:
-#         print('Port supposed to be integer value, not {}. Now we\'ll use 7777'.format(value))
-#         return False
-#     else:
-#         if port > 65534 or port < 1024:
-#             print('Port supposed to be between 1024 and 65535, not {}. Now we\'ll use 7777'.format(value))
-#             return False
-#         else:
-#             return True
-
-
 class Client():
     def __init__(self, socket):
         self.socket = socket
